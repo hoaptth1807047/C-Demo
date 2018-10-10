f0a8f05b47286ef08ef973f8bc5ac22180e87baa
Pham Thanh Hoa <hoaptth1807047@fpt.edu.vn>
2018-10-10 23:29:57 +0700

    Max-Min

--- main.c
+++ main.c
@@ -1,44 +1,61 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-float tinhTienLuongThang13(float soNamKinhNghiem, float soNamLamViec) {
-    float mucThuong;
-    float heSo;
-    if (soNamKinhNghiem < 2) {
-        mucThuong = 10000000;
-    } else if (soNamKinhNghiem > 2 && soNamKinhNghiem <= 5) {
-        mucThuong = 20000000;
-    } else {
-        mucThuong = 30000000;
-    }
-    if (soNamLamViec < 1) {
-        heSo = 0.3;
-    } else if (soNamLamViec > 1 && soNamLamViec <= 2) {
-        heSo = 0.5;
-    } else if (soNamLamViec > 2 && soNamLamViec <= 5) {
-        heSo = 1;
-    } else {
-        heSo = 2;
-    }
-    return mucThuong * heSo;
-}
-
 int main() {
-    float soNamKinhNghiem;
-    float soNamLamViec;
-    char luachon;
     while (1 == 1) {
-        printf("Vui lòng nhập số năm kinh nghiệm: \n");
-        scanf("%f", &soNamKinhNghiem);
-        printf("Vui lòng nhập số năm làm việc: \n");
-        scanf("%f", &soNamLamViec);
-        printf("Số tiền thưởng tháng 13 là: %.2f\n", tinhTienLuongThang13(soNamKinhNghiem, soNamLamViec));
-        printf("Bạn có muốn tính toán thêm không (y/n)?: ");
-        scanf("%s", &luachon);
+        int soluongphantu, mang[soluongphantu], findMaxValue, findMinValue;
+        while (1 == 1) {
+            printf("Vui lòng nhập số lượng phần tử:\n");
+            scanf("%d", &soluongphantu);
+            if (soluongphantu > 10) {
+                printf("Vui lòng nhập số lượng phần tử nhỏ hơn bằng 10.\n");
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 0; i < soluongphantu; ++i) {
+            printf("Vui lòng nhập phần tử thứ %d: \n", i + 1);
+            scanf("%d", &mang[i]);
+        }
+
+        printf("Thông tin của các phần tử trong mảng: \n");
+        for (int j = 0; j < soluongphantu; ++j) {
+            printf("Phần tử thứ %d là: %d\n", j + 1, mang[j]);
+        }
+
+        findMaxValue = findMinValue = mang[0];
+        for (int k = 0; k < soluongphantu; ++k) {
+            if (findMaxValue < mang[k]) {
+                findMaxValue = mang[k];
+            }
+            if (findMinValue > mang[k]) {
+                findMinValue = mang[k];
+            }
+        }
+        printf("Vui lòng nhập lựa chọn của bạn: \n");
+        printf("1.Hiển thị giá trị lớn nhất trong mảng. \n");
+        printf("2.Hiển thị giá trị nhỏ nhất trong mảng. \n");
+
+        int luachon;
+        scanf("%d", &luachon);
+        switch (luachon) {
+            case 1:
+                printf("Giá trị lớn nhất của mảng là:%d\n", findMaxValue);
+                break;
+            case 2:
+                printf("Giá trị nhỏ nhất của mảng là:%d\n", findMinValue);
+                break;
+        }
+
+        char choice;
+        printf("Bạn có muốn tiếp tục không(y/n)?");
+        scanf("%s", &choice);
         if (luachon == 'n' || luachon == 'N') {
             printf("Hẹn gặp lại.");
             break;
         }
+        break;
     }
     return 0;
 }
