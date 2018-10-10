d2534ddde8e37dfe476421e461703a14ea430f2a
Pham Thanh Hoa <hoaptth1807047@fpt.edu.vn>
2018-10-10 20:08:32 +0700

    Money

--- main.c
+++ main.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include<stdlib.h>
-#include <math.h>
 
-double tinhNuaChuViTamGiac(double canhThuNhat, double canhThuHai, double canhThuBa) {
-    return (canhThuNhat + canhThuHai + canhThuBa) / 2;
+float tinhTienLuongThang13(float soNamKinhNghiem, float soNamLamViec) {
+    float mucThuong;
+    float heSo;
+    if (soNamKinhNghiem < 2) {
+        mucThuong = 10000000;
+    } else if (soNamKinhNghiem > 2 && soNamKinhNghiem <= 5) {
+        mucThuong = 20000000;
+    } else {
+        mucThuong = 30000000;
+    }
+    if (soNamLamViec > 1) {
+        heSo = 0.3;
+    } else if (soNamLamViec > 1 && soNamLamViec <= 2) {
+        heSo = 0.5;
+    } else if (soNamLamViec > 2 && soNamLamViec <= 5) {
+        heSo = 1;
+    } else {
+        heSo = 2;
+    }
+    return mucThuong * heSo;
 }
 
 int main() {
-    double canhThuNhat, canhThuHai, canhThuBa;
-    printf("Vui lòng nhập cạnh thứ nhất:\n");
-    scanf("%lf", &canhThuNhat);
-    printf("Vui lòng nhập cạnh thứ hai:\n");
-    scanf("%lf", &canhThuHai);
-    printf("Vui lòng nhập cạnh thứ ba:\n");
-    scanf("%lf", &canhThuBa);
-    if (canhThuNhat <= 0 || canhThuHai <= 0 || canhThuNhat <= 0) {
-        printf("Độ dài một cạnh không thể nhỏ hơn bằng 0.\n");
-    } else if (canhThuNhat + canhThuHai <= canhThuBa || canhThuNhat + canhThuBa <= canhThuHai ||
-               canhThuHai + canhThuBa <= canhThuNhat) {
-        printf("Tổng hai cạnh của một tam giác luôn phải lớn hơn cạnh còn lại (Theo bất đẳng thức tam giác)\n");
-    } else {
-        printf("Nửa chu vi tam giác là: %.2lf", tinhNuaChuViTamGiac(canhThuNhat, canhThuHai, canhThuBa));
+    float soNamKinhNghiem;
+    float soNamLamViec;
+    char luachon;
+    while (1 == 1) {
+        printf("Vui lòng nhập số năm kinh nghiệm: \n");
+        scanf("%f", &soNamKinhNghiem);
+        printf("Vui lòng nhập số năm làm việc: \n");
+        scanf("%f", &soNamLamViec);
+        printf("Số tiền thưởng tháng 13 là: %.2f\n", tinhTienLuongThang13(soNamKinhNghiem, soNamLamViec));
+        printf("Bạn có muốn tính toán thêm không (y/n)?: ");
+        scanf("%s", &luachon);
+        if (luachon == 'n' || luachon == 'N') {
+            printf("Hẹn gặp lại.");
+            break;
+        }
     }
     return 0;
 }
